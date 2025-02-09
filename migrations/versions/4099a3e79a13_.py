17a0cf8711bc300f0179e1087db1d4705958d8a2
William <wcathey1604@gmail.com>
2025-02-09 17:29:47 -0600

    attempt

---
+++ migrations/versions/4099a3e79a13_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4099a3e79a13
+Revises: 
+Create Date: 2025-02-09 17:28:36.721075
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4099a3e79a13'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
